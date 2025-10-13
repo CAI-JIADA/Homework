048c21e238b66098ab8d718f95ffc5eaf8ffdfd0
DaDa-043 <41343145@nfu.edu.tw>
2025-10-13 21:47:43 +0800

    Add files via upload

---
+++ homework1/src/Ackermann.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int Ackermann(int m,int n){//遞迴函式
+	if (m == 0) return n + 1;
+	else if (n == 0)return Ackermann(m - 1, 1);
+	else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+int main() {
+	int m, n;
+	cout << "輸入Ackermann的m和n:";
+	cin >> m>> n;//輸入Ackermann的m和n
+	cout << Ackermann(m, n) << endl;//呼叫遞迴函式
+	return 0;
+}
